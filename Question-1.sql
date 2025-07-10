cf6688f9ef2cee21b3e260d6c8511f3f161fe7d1
Gunasai <141109306+gunasai197@users.noreply.github.com>
2025-07-10 11:51:30 +0530

    big_countries

--- Question-1.sql
+++ Question-1.sql
@@ -50,3 +50,27 @@ Find the names of the customer that are not referred by the customer with id = 2
 Return the result table in any order.*/
 select name from customer where referred_id is NULL or referred_id not in 2;
 
+/* 595. Big Countries
+Table: World
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| name        | varchar |
+| continent   | varchar |
+| area        | int     |
+| population  | int     |
+| gdp         | bigint  |
++-------------+---------+
+name is the primary key (column with unique values) for this table.
+Each row of this table gives information about the name of a country, the continent to which it belongs, its area, the population, and its GDP value.
+ 
+A country is big if:
+
+it has an area of at least three million (i.e., 3000000 km2), or
+it has a population of at least twenty-five million (i.e., 25000000).
+Write a solution to find the name, population, and area of the big countries.
+
+Return the result table in any order. */
+select name, population, area from World where area >= 3000000 or population >= 25000000;
+
