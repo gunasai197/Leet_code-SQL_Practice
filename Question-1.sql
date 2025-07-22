3e9206170d1803df24044bb639c7a5ff01b4723b
Gunasai <141109306+gunasai197@users.noreply.github.com>
2025-07-22 19:40:23 +0530

    1729_find_followers_count

--- Question-1.sql
+++ Question-1.sql
@@ -74,3 +74,26 @@ Write a solution to find the name, population, and area of the big countries.
 Return the result table in any order. */
 select name, population, area from World where area >= 3000000 or population >= 25000000;
 
+/* 1729. find followers count
+Table: Followers
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| user_id     | int  |
+| follower_id | int  |
++-------------+------+
+(user_id, follower_id) is the primary key (combination of columns with unique values) for this table.
+This table contains the IDs of a user and a follower in a social media app where the follower follows the user.
+ 
+
+Write a solution that will, for each user, return the number of followers.
+
+Return the result table ordered by user_id in ascending order. */
+
+select user_id, count(follower_id) followers_count from followers 
+group by user_id 
+order by user_id asc;
+
+
+
