e70ae4fc18cfc48c8737715ea59a5e462378ebce
gunasai197 <gunasai197@gmail.com>
2025-07-25 16:48:38 +0530

    1327

--- Question-1.sql
+++ Question-1.sql
@@ -116,5 +116,41 @@ Return the result table in any order.*/
 select teacher_id, count(DISTINCT subject_id) cnt from teacher
 group by teacher_id;
 
+/* 1327. List the products ordered in a period link ---> https://leetcode.com/problems/list-the-products-ordered-in-a-period/description/
+Table: Products
+
++------------------+---------+
+| Column Name      | Type    |
++------------------+---------+
+| product_id       | int     |
+| product_name     | varchar |
+| product_category | varchar |
++------------------+---------+
+product_id is the primary key (column with unique values) for this table.
+This table contains data about the company's products.
+ 
+
+Table: Orders
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| product_id    | int     |
+| order_date    | date    |
+| unit          | int     |
++---------------+---------+
+This table may have duplicate rows.
+product_id is a foreign key (reference column) to the Products table.
+unit is the number of products ordered in order_date.
+ 
+
+Write a solution to get the names of products that have at least 100 units ordered in February 2020 and their amount.
+Return the result table in any order. */
+
+select product_name, sum(unit) unit from products prod, orders o 
+where prod.product_id = o.product_id and order_date between '2020-02-01' and '2020-02-29'
+group by product_name having sum(unit) >= 100;
+
+
 
 
