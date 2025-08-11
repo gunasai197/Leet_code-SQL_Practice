9fb8e139388a26cb1f4a4a6c74ccc3149e46f8ec
gunasai197 <gunasai197@gmail.com>
2025-08-11 20:19:17 +0530

    employee bonus

--- Question-1.sql
+++ Question-1.sql
@@ -378,3 +378,70 @@ Return the result table sorted in any order.*/
 select v.customer_id,count(v.visit_id) as count_no_trans from visits v where 
 v.visit_id NOT IN ( SELECT T.visit_id FROM Transactions T
 WHERE T.amount IS NOT NULL) GROUP BY v.customer_id ORDER BY v.customer_id;
+
+/*Table: Employee
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| empId       | int     |
+| name        | varchar |
+| supervisor  | int     |
+| salary      | int     |
++-------------+---------+
+empId is the column with unique values for this table.
+Each row of this table indicates the name and the ID of an employee in addition to their salary and the id of their manager.
+ 
+
+Table: Bonus
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| empId       | int  |
+| bonus       | int  |
++-------------+------+
+empId is the column of unique values for this table.
+empId is a foreign key (reference column) to empId from the Employee table.
+Each row of this table contains the id of an employee and their respective bonus.
+ 
+
+Write a solution to report the name and bonus amount of each employee with a bonus less than 1000.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++-------+--------+------------+--------+
+| empId | name   | supervisor | salary |
++-------+--------+------------+--------+
+| 3     | Brad   | null       | 4000   |
+| 1     | John   | 3          | 1000   |
+| 2     | Dan    | 3          | 2000   |
+| 4     | Thomas | 3          | 4000   |
++-------+--------+------------+--------+
+Bonus table:
++-------+-------+
+| empId | bonus |
++-------+-------+
+| 2     | 500   |
+| 4     | 2000  |
++-------+-------+
+Output: 
++------+-------+
+| name | bonus |
++------+-------+
+| Brad | null  |
+| John | null  |
+| Dan  | 500   |
++------+-------+*/
+
+select emp.name,bon.bonus from employee emp left join bonus bon
+on emp.empid = bon.bonus where bon.bonus < 1000 or bon.bonus is NULL:
+ 
