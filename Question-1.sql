a5d42c8a084182f532bf6eb38dbbfa4b27e2d9b8
gunasai197 <gunasai197@gmail.com>
2025-08-12 23:52:30 +0530

    report to each emp

--- Question-1.sql
+++ Question-1.sql
@@ -441,3 +441,29 @@ Output:
 
 select emp.name,bon.bonus from employee emp left join bonus bon
 on emp.empid = bon.bonus where bon.bonus < 1000 or bon.bonus is NULL:
+/* 1731. The Number of employees which report to each employee
+Table: Employees
+
++-------------+----------+
+| Column Name | Type     |
++-------------+----------+
+| employee_id | int      |
+| name        | varchar  |
+| reports_to  | int      |
+| age         | int      |
++-------------+----------+
+employee_id is the column with unique values for this table.
+This table contains information about the employees and the id of the manager they report to. Some employees do not report to anyone (reports_to is null). 
+ 
+
+For this problem, we will consider a manager an employee who has at least 1 other employee reporting to them.
+
+Write a solution to report the ids and the names of all managers, the number of employees who report directly to them, and the average age of the reports rounded to the nearest integer.
+
+Return the result table ordered by employee_id.
+
+The result format is in the following example.*/
+select e.employee_id,e.name,count(e1.employee_id) as reports_count
+round(avg(e1.age)) as average_age from Employees e join Employees e1
+on e.employee_id = e1.reports_to group by e.employee_id,e.name
+order by e.employee_id;
