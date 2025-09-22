b1c40e862997192507ef0004e79e8673258ba04d
gunasai197 <gunasai197@gmail.com>
2025-09-22 19:03:38 +0530

    178-rank scores

--- Question-1.sql
+++ Question-1.sql
@@ -1164,3 +1164,41 @@ Output:
 Explanation: There are no single numbers in the input table so we return null. */
 
 select max(num) as num from (select num from MyNumbers group by num having count(num) = 1);
+
+/* 178. Rank Scores 
+Write a solution to find the rank of the scores. The ranking should be calculated according to the following rules:
+
+The scores should be ranked from the highest to the lowest.
+If there is a tie between two scores, both should have the same ranking.
+After a tie, the next ranking number should be the next consecutive integer value. In other words, there should be no holes between ranks.
+Return the result table ordered by score in descending order.
+
+The result format is in the following example.
+
+Example 1:
+
+Input: 
+Scores table:
++----+-------+
+| id | score |
++----+-------+
+| 1  | 3.50  |
+| 2  | 3.65  |
+| 3  | 4.00  |
+| 4  | 3.85  |
+| 5  | 4.00  |
+| 6  | 3.65  |
++----+-------+
+Output: 
++-------+------+
+| score | rank |
++-------+------+
+| 4.00  | 1    |
+| 4.00  | 1    |
+| 3.85  | 2    |
+| 3.65  | 3    |
+| 3.65  | 3    |
+| 3.50  | 4    |
++-------+------+*/
+
+select score,DENSE_RANK() OVER (order by score desc) AS rank from Scores;
