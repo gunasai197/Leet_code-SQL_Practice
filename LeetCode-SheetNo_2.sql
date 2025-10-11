79d29d65bd69b95a376e7ef7d27aa586dc30a8c7
gunasai197 <gunasai197@gmail.com>
2025-10-11 11:38:17 +0530

    swap salary

--- LeetCode-SheetNo_2.sql
+++ LeetCode-SheetNo_2.sql
@@ -341,3 +341,36 @@ The salary of employee 2 is missing.*/
 select employee_id from Salaries where employee_id not in (select employee_id from employees)
 UNION
 select employee_id from Employees where employee_id not in (select employee_id from Salaries);
+
+/* 627. swap salary, Write a solution to swap all 'f' and 'm' values (i.e., change all 'f' values to 'm' and vice versa)
+with a single update statement and no intermediate temporary tables.
+Note that you must write a single update statement, do not write any select statement for this problem.
+
+The result format is in the following example.
+
+Example 1:
+
+Input: 
+Salary table:
++----+------+-----+--------+
+| id | name | sex | salary |
++----+------+-----+--------+
+| 1  | A    | m   | 2500   |
+| 2  | B    | f   | 1500   |
+| 3  | C    | m   | 5500   |
+| 4  | D    | f   | 500    |
++----+------+-----+--------+
+Output: 
++----+------+-----+--------+
+| id | name | sex | salary |
++----+------+-----+--------+
+| 1  | A    | f   | 2500   |
+| 2  | B    | m   | 1500   |
+| 3  | C    | f   | 5500   |
+| 4  | D    | m   | 500    |
++----+------+-----+--------+
+Explanation: 
+(1, A) and (3, C) were changed from 'm' to 'f'.
+(2, B) and (4, D) were changed from 'f' to 'm'.*/
+
+update Salary set sex = case when 'm' then 'f' else 'm' end;
